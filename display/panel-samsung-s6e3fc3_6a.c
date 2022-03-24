dd1ece58b89313efbe6d66d159a7f248d5ec247b
linpeter <linpeter@google.com>
2022-03-24 16:38:31 +0800

    drm: samsung: skip to update dimming in lp state
    Stored the dimming flag in lp state. Updated dimming
    when exit lp state.
    
    Bug: 220027286
    Test: check dimming on if on/aod/off state
    Signed-off-by: linpeter <linpeter@google.com>
    Change-Id: Ifdfdc5aa978d7c9d1473467b9646c7d2547a293a

--- display/panel-samsung-s6e3fc3_6a.c
+++ display/panel-samsung-s6e3fc3_6a.c
@@ -233,6 +233,7 @@ static void s6e3fc3_6a_set_nolp_mode(struct exynos_panel *ctx,
 		return;
 
 	EXYNOS_DCS_WRITE_TABLE(ctx, display_off);
+	/* backlight control and dimming */
 	s6e3fc3_6a_update_wrctrld(ctx);
 	usleep_range(delay_us, delay_us + 10);
 	EXYNOS_DCS_WRITE_TABLE(ctx, display_on);
@@ -346,7 +347,13 @@ static void s6e3fc3_6a_set_hbm_mode(struct exynos_panel *exynos_panel,
 static void s6e3fc3_6a_set_dimming_on(struct exynos_panel *exynos_panel,
 				 bool dimming_on)
 {
+	const struct exynos_panel_mode *pmode = exynos_panel->current_mode;
+
 	exynos_panel->dimming_on = dimming_on;
+	if (pmode->exynos_mode.is_lp_mode) {
+		dev_info(exynos_panel->dev,"in lp mode, skip to update");
+		return;
+	}
 
 	s6e3fc3_6a_update_wrctrld(exynos_panel);
 }
